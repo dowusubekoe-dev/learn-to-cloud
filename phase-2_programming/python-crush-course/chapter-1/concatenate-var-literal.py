9f11cf2e3cb7e19e62f7788acb6880cba5fb7ecc
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-11-14 17:13:14 +0000

    concatenate a variable with a string

---
+++ phase-2_programming/python-crush-course/chapter-1/concatenate-var-literal.py
@@ -0,0 +1,8 @@
+# Concatenate a variable and a literal using (+) sign
+
+
+# Note that concatenating a variable and a string will give you a <stdin> error or invalid syntax
+
+prefix = 'Py'
+
+print(prefix + 'thon')
