664086f822abb5e64ac259dfe6a412b631689858
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-11-14 14:16:30 +0000

    print different variables

---
+++ phase-2_programming/python-crush-course/chapter-1/print-different-variables.py
@@ -0,0 +1,15 @@
+# Print the types of different variables
+
+# Define integers, floats, and strings.
+age = 25
+
+height = 5.9
+
+name = "John"
+
+# Define a boolean variable.
+is_cloud_engineer = True
+
+# Print the types of different variables.
+
+print(type(age), type(height), type(name), type(is_cloud_engineer))
