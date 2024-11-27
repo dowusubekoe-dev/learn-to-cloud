294745fd47541db98f2458ea2e0bc959fa834496
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-11-27 16:35:17 +0000

    escaping single quotes

--- phase-2_programming/python-crush-course/chapter-2/famous_person.py
+++ phase-2_programming/python-crush-course/chapter-2/famous_person.py
@@ -2,10 +2,8 @@
 
 famous_person = "Denzel Washington"
 message = f'{famous_person} once said, "I say luck is when an opportunity comes along and you\'re prepared for it."'
-
 print(message)
 
 # Correcting the second assignment
 message = famous_person + ' once said, "I say luck is when an opportunity comes along and you\'re prepared for it."'
-
 print(message)
