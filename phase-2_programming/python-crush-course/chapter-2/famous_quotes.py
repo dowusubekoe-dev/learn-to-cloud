d3af07113c05e61788cc9dbef12b95158bba3676
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-11-27 16:44:52 +0000

    escaping single quote format

---
+++ phase-2_programming/python-crush-course/chapter-2/famous_quotes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+famous_quote = "I say luck is when an opportunity comes along and you're prepared for it."
+print(famous_quote)
+print('Denzel Washington once said, "' + famous_quote + '"')
+print(f'Denzel Washington once said, "{famous_quote}"')
