529174bc39210fda3a8d8eddac5dd13d72d0462e
Derek Owusu Bekoe <dowusubekoe@gmail.com>
2024-11-27 19:23:54 +0000

    use of removesuffix and removeprefix methods

---
+++ phase-2_programming/python-crush-course/chapter-2/file_extensions.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+filename = "python_notes.txt"
+
+filename_without_extension = filename.removesuffix(".txt")
+print(filename_without_extension)
+
+filename_without_extension = filename.removeprefix("p")
+print(filename_without_extension)
